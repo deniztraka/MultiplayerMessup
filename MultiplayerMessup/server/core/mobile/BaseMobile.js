9765867382fc9ee96a4455524deb01078aecb4b3
deniz <deniz.traka@hotmail.com>
2016-10-06 12:27:26 +0300

    some code refactoring.

---
+++ MultiplayerMessup/server/core/mobile/BaseMobile.js
@@ -0,0 +1,39 @@
+﻿var config = require('../../common/config.js');
+var constants = require('../../common/constants.js');
+var utils = require('../../common/utils.js');
+var p2 = require('p2');
+
+module.exports = BaseMobile;
+
+function BaseMobile(bodyOptions,name,speed,bodyType) {
+    var selfOptions = bodyOptions;
+    p2.Body.call(this, selfOptions);
+
+    this.zone = {
+        x: Math.floor(this.position[0] / config.server.zoneSize.width),
+        y: Math.floor(this.position[1] / config.server.zoneSize.height),
+    };
+    this.movementStates = {
+        isMovingUp: false,
+        isMovingDown: false,
+        isMovingLeft: false,
+        isMovingRight: false,
+        isRunning: false
+    };
+
+    this.pName = name;
+    this.pSpeed = speed;
+    this.bodyType = bodyType;
+
+    this.clientInfo = {
+        id: this.id,
+        name: this.pName,
+        position: {
+            x: this.position[0],
+            y: this.position[1]
+        }
+    };
+};
+
+BaseMobile.prototype = new Object(p2.Body.prototype);
+BaseMobile.prototype.constructor = BaseMobile;
--- MultiplayerMessup/server/core/mobile/player.js
+++ MultiplayerMessup/server/core/mobile/player.js
@@ -1,51 +1,26 @@
 ﻿var config = require('../../common/config.js');
 var constants = require('../../common/constants.js');
 var utils = require('../../common/utils.js');
+var BaseMobile = require('./BaseMobile.js');
 var p2 = require('p2');
 
 module.exports = Player;
 
 function Player(name) {    
-    p2.Body.call(this, {
+    BaseMobile.call(this, {
         mass: constants.game.player.mass,
-        position: [utils.math.randomInt(0, config.game.bounds.width), utils.math.randomInt(0, config.game.bounds.height)],
-        angularVelocity: 200
-    });
+        position: [utils.math.randomInt(0, config.game.bounds.width), utils.math.randomInt(0, config.game.bounds.height)]
+    }, name, constants.game.player.speed, constants.game.player.type);
     var playerShape = new p2.Circle({ radius: constants.game.player.radius });
     //playerShape.collisionGroup = Math.pow(2, 0);
     //playerShape.collisionMask = Math.pow(2, 0);
     this.addShape(playerShape);
     this.damping = 1;
-
     this.mousePosition = {
         x: 0,
         y: 0
     };
-    this.zone = {
-        x: Math.floor(this.position[0] / config.server.zoneSize.width),
-        y: Math.floor(this.position[1] / config.server.zoneSize.height),
-    };
-    this.movementStates = {
-        isMovingUp: false,
-        isMovingDown: false,
-        isMovingLeft: false,
-        isMovingRight: false,
-        isRunning: false
-    };
-    
-    this.pName = name;
-    this.pSpeed = constants.game.player.speed;
-    this.bodyType = constants.game.player.type;
-
-    this.clientInfo = {
-        id: this.id,
-        name: this.pName,
-        position: {
-            x: this.position[0],
-            y: this.position[1]
-        }
-    };
 }
 
-Player.prototype = new Object(p2.Body.prototype);
+Player.prototype = new Object(BaseMobile.prototype);
 Player.prototype.constructor = Player;
